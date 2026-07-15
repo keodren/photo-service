575904b4ed7010fd08d80ab45a2b473f6ed97c4e
Eder G <you@example.com>
2026-07-14 23:12:24 -0600

    Completed the GET tests for this endpoint so far.

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -11,7 +11,7 @@ import worker from "../src/index";
 // `Request` to pass to `worker.fetch()`.
 const IncomingRequest = Request<unknown, IncomingRequestCfProperties>;
 
-describe("Photo Service - Unit Tests", () => {
+describe("Photo Service API - Unit Tests", () => {
 	// Testing a non-existing endpoint
 	it("returns a 404 if a non-existent endpoint is called", async () => {
 		const response = await SELF.fetch('http://www.example.com/invalid-endpoint');
@@ -19,10 +19,40 @@ describe("Photo Service - Unit Tests", () => {
     });
 
 	// Testing all GET APIs
-	describe("Testing - GET /images", () => {
+	describe("GET /images", () => {
 		it("should return a 200 OK response", async () => {
 			const response = await SELF.fetch('http://www.example.com/images');
 			expect(response.status).toEqual(200);
+		});
+
+		it("should return images in response", async () => {
+			const response = await SELF.fetch('http://www.example.com/images');
+			const json = await response.json();
+			expect(json).toEqual(
+				expect.arrayContaining([
+					expect.objectContaining(
+						{ id: 1, url: 'https://foo.com/img1', author: 'bart Simpson'} //example located at ../src/data/image_store.ts
+					)
+				])
+			);
+		});
+
+		it("should return a set number of images if a count is provided", async () => {
+			const response = await SELF.fetch('http://www.example.com/images?count=2');
+			const json = await response.json();
+			expect(json).toHaveLength(2);
+		});
+
+		//This test was not included in the book, but the use case does exist.
+		//I'm going to query for a specific single image
+		it("should return a specific image when ID is passed in the URL", async () => {
+			const response = await SELF.fetch('http://www.example.com/images/2');
+			const json = await response.json();
+			expect(json).toEqual(
+				expect.objectContaining(
+					{ id: 2, url: 'https://baz.com/img2', author: 'Larry Lobster'} //example located at ../src/data/image_store.ts
+				)
+			);
 		});
 	});
 });
