0fd9b817bf9dc0424c260dfbab307cc486ac003e
Eder G <you@example.com>
2026-07-18 23:57:52 -0600

    Modified get_single_image.ts to query D1 now.

--- src/handlers/get_single_image.ts
+++ src/handlers/get_single_image.ts
@@ -1,14 +1,39 @@
-import { ALL_IMAGES } from '../data/image_store';
+//import { ALL_IMAGES } from '../data/image_store';
 import { IRequest } from 'itty-router';
+import { Env } from '../env';
 
-const getSingleImage = (request: IRequest) => {
-    let image = ALL_IMAGES.find(i => i.id.toString() === request.params.id);
+//const getSingleImage = (request: IRequest) => {
+const getSingleImage = async (request: IRequest, env: Env): Promise<Response> => {
+    //let image = ALL_IMAGES.find(i => i.id.toString() === request.params.id);
+    let result;
 
-    if(!image) {
-        return new Response('Not Found!', { status: 404 });
+    //if(!image) {
+    //    return new Response('Not Found!', { status: 404 });
+    //}
+
+    try {
+        result = await env.DB.prepare(
+            `SELECT i.*, c.display_name AS category_display_name
+               FROM images i
+              INNER JOIN image_categories c ON i.category_id = c.id
+              WHERE i.id = ?1`
+        )
+        .bind(request.params.id)
+        .first()
+    } catch (e) {
+        let message;
+        if (e instanceof Error) message = e.message;
+
+        console.log({message: message});
+
+        return new Response('Error', {status: 500})
     }
 
-    return new Response(JSON.stringify(image), {
+    if (!result) {
+        return new Response('Not Found', {status: 404})
+    }
+
+    return new Response(JSON.stringify(result), {
         headers: { 'Content-Type': 'application/json' }
     });
 };
